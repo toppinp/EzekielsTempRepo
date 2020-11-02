0df92f3ae7dcadab5dc4a8fc0b0f2156da67de77
Patrick Toppin <69924183+toppinp@users.noreply.github.com>
2020-11-02 09:26:55 -0600

    Initial Repository

---
+++ ProjectStep10/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjectStep10.Models;
+
+namespace ProjectStep10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Account a)
+        {
+            if (ModelState.IsValid)
+            {
+                if (a.password == a.confirmPassword)
+                {
+                    return View("UserHome", a);
+                    // return RedirectToAction("UserHome", a); // Requires making a method for the View.
+                }
+                else
+                {
+                    return View(a);
+                }
+            }
+            else
+            {
+                return View(a);
+            }
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult EditAccount()
+        {
+            // Go to database with id and get account with that id.
+            // This is a mock up DB.
+
+            Account a = new Account();
+            a.userId = 1;
+            a.name = "Patrick Toppin";
+            a.email = "patrick.toppin@gmail.com";
+            a.phone = "9158016325";
+            a.streetAddress = "516 Fieldstone Dr.";
+            a.city = "Gatesville";
+            return View(a);
+        }
+
+        [HttpPost]
+        public IActionResult EditAccount(Account a)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("UserHome", a);
+            }
+            else
+            {
+                return View(a);
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProjectStep10/Models/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectStep10.Models
+{
+    public class Account
+    {
+        // F i e l d s   &   P r o p e r t i e s
+
+        public int userId { get; set; }
+
+        [Required(ErrorMessage="Name Is Required")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Phone Is Required")]
+        public string phone { get; set; }
+
+        [Required(ErrorMessage = "Street Address Is Required")]
+        public string streetAddress { get; set; }
+
+        [Required(ErrorMessage = "City Is Required")]
+        public string city { get; set; }
+
+        [Required(ErrorMessage = "Email Is Required")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "The Passwords Must Match")]
+        public string confirmPassword { get; set; }
+
+        // C o n s t u c t o r s
+
+
+        // M e t h o d s 
+
+
+    }
+}
